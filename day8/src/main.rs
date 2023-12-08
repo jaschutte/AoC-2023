6ab993a232b595c5a5ad8aae940991fcb3e37b42
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-12-08 22:52:23 +0100

    Day 8, excluding LCM

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -4,34 +4,158 @@ fn one(content: String) -> u64 {
     let steps = steps
         .chars()
         .map(|c| match c {
-            'L' => 1,
-            'R' => 2,
+            'L' => 0_usize,
+            'R' => 1_usize,
             _ => panic!("Left or right..???? WHat is thiiiiiiiis????"),
         })
         .collect::<Vec<_>>();
 
-    let nodes = nodes
+    let ref_nodes = nodes
         .lines()
         .filter_map(|line| {
             let (source, targets) = line.split_once(" = ")?;
             let (left, right) = targets.split_once(", ")?;
             let left = left.trim_start_matches("(");
-            let right = right.trim_start_matches(")");
+            let right = right.trim_end_matches(")");
 
             Some([source, left, right])
         })
         .collect::<Vec<_>>();
 
-    let mut walker = steps.iter().cycle()
-    loop {
-
+    struct Node<'a> {
+        directions: [usize; 2],
+        label: &'a str,
     }
 
+    let nodes = ref_nodes
+        .iter()
+        .map(|this_node| {
+            let left = ref_nodes
+                .iter()
+                .position(|find| find[0] == this_node[1])
+                .expect("Invalid");
+            let right = ref_nodes
+                .iter()
+                .position(|find| find[0] == this_node[2])
+                .expect("Invalid");
+            Node {
+                directions: [left, right],
+                label: this_node[0],
+            }
+        })
+        .collect::<Vec<Node>>();
 
-    0
+    let mut walker = steps.iter().cycle();
+    let mut current_node = nodes
+        .iter()
+        .find(|n| n.label == "AAA")
+        .expect("Invalid Input");
+    let mut iteration = 0;
+    loop {
+        iteration += 1;
+        let direction = *walker
+            .next()
+            .expect("We're cycling, so how is this failing?");
+
+        let target_node = &nodes[current_node.directions[direction]];
+        if target_node.label == "ZZZ" {
+            break;
+        }
+
+        current_node = target_node;
+    }
+    iteration
 }
 
 fn two(content: String) -> u64 {
+    let (steps, nodes) = content.split_once("\n\n").expect("Invalid Input");
+
+    let steps = steps
+        .chars()
+        .map(|c| match c {
+            'L' => 0_usize,
+            'R' => 1_usize,
+            _ => panic!("Left or right..???? WHat is thiiiiiiiis????"),
+        })
+        .collect::<Vec<_>>();
+
+    let ref_nodes = nodes
+        .lines()
+        .filter_map(|line| {
+            let (source, targets) = line.split_once(" = ")?;
+            let (left, right) = targets.split_once(", ")?;
+            let left = left.trim_start_matches("(");
+            let right = right.trim_end_matches(")");
+
+            Some([source, left, right])
+        })
+        .collect::<Vec<_>>();
+
+    struct Node<'a> {
+        directions: [usize; 2],
+        label: &'a str,
+    }
+
+    let nodes = ref_nodes
+        .iter()
+        .map(|this_node| {
+            let left = ref_nodes
+                .iter()
+                .position(|find| find[0] == this_node[1])
+                .expect("Invalid");
+            let right = ref_nodes
+                .iter()
+                .position(|find| find[0] == this_node[2])
+                .expect("Invalid");
+            Node {
+                directions: [left, right],
+                label: this_node[0],
+            }
+        })
+        .collect::<Vec<Node>>();
+
+    let mut walker = steps.iter().cycle();
+    let mut current_nodes: Vec<&Node<'_>> =
+        nodes.iter().filter(|n| n.label.ends_with("A")).collect();
+    let mut patterns: Vec<Vec<u64>> = current_nodes.iter().map(|_| vec![]).collect();
+    let mut found: Vec<Option<u64>> = current_nodes.iter().map(|_| None).collect();
+
+    let mut iteration = 0_u64;
+    loop {
+        iteration += 1;
+        let direction = *walker
+            .next()
+            .expect("We're cycling, so how is this failing?");
+
+        for (index, current_node) in current_nodes.iter_mut().enumerate() {
+            *current_node = &nodes[current_node.directions[direction]];
+
+            if current_node.label.ends_with("Z") && found[index].is_none() {
+                patterns[index].push(iteration);
+                found[index] = patterns[index]
+                    .starts_with(&patterns[index][(patterns[index].len() / 2)..])
+                    .then_some(iteration);
+            }
+        }
+
+        if found.iter().all(|found| found.is_some()) {
+            break;
+        }
+    }
+
+    println!(
+        r"
+So, I got no way to really calculate the LCM, so please go to this site and fill in the following numbers:
+https://www.calculatorsoup.com/calculators/math/lcm.php
+
+Numbers:
+{}
+",
+        found.iter().fold(String::new(), |str, num| {
+            format!("{str}{} ", num.expect("IMPOSSIBLE!"))
+        })
+    );
+
     0
 }
 
